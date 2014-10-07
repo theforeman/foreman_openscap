0058f4a92b13265efe74bc96ba46a2a73b17e06e
Šimon Lukašík <slukasik@redhat.com>
2014-10-07 11:11:20 +0200

    Introduce API for ARF upload.

---
+++ app/controllers/api/v2/openscap/arf_controller.rb
@@ -0,0 +1,42 @@
+#
+# Copyright (c) 2014 Red Hat Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 3 (GPLv3). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
+# along with this software; if not, see http://www.gnu.org/licenses/gpl.txt
+#
+
+module Api
+  module V2
+    module Openscap
+
+      class ArfController < V2::BaseController
+        include Api::Version2
+        include Foreman::Controller::SmartProxyAuth
+
+        api :POST, "/arf/:cname/:policy_name/:date", N_("Upload an ARF report")
+        param :cname, :identifier, :required => true
+        param :policy_name, :identifier, :required => true
+        param :date, :identifier, :required => true
+
+        def create
+          logger.error params
+          render :json => { "result" => "not implemented" }
+        end
+
+        def check_content_type
+          # Well, this is unfortunate. Parent class asserts that content-type is
+          # application/json. While we want to have content-type text/xml. We
+          # also need the content-encoding to equal with x-bzip2. However, when
+          # the framework sees text/xml, it will rewrite it to application/xml.
+          # What's worse, a framework will try to parse body as an utf8 string,
+          # no matter what content-encoding says. Let's pass content-type x-bzip2
+          # and move forward.
+          super unless request.content_type.end_with? "x-bzip2"
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
 
   match 'new_action', :to => 'foreman_openscap/hosts#new_action'
 
+  namespace :api do
+    scope "(:apiv)", :module => :v2, :defaults => {:apiv => 'v2'},
+          :apiv => /v1|v2/, :constraints => ApiConstraints.new(:version => 2) do
+      namespace :openscap do
+        post 'arf/:cname/:policy/:date', \
+              :constraints => { :cname => /[^\/]+/ }, :to => 'arf#create'
+      end
+    end
+  end
 end
