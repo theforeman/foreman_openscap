b26728dc16eb646ce3412be5fa619797b38e4fae
Šimon Lukašík <slukasik@redhat.com>
2014-10-17 13:47:07 +0200

    Introduce AuditableHost
    as a link between Foreman's Host and SCAPtimony's Asset.

---
+++ app/models/scaptimony/asset.rb
@@ -0,0 +1,28 @@
+#
+# Copyright (c) 2014 Red Hat Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 3 (GPLv3). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
+# along with this software; if not, see http://www.gnu.org/licenses/gpl.txt
+#
+
+module ::Scaptimony
+  class AuditableHost < ActiveRecord::Base
+    # Links Foreman's Host table with SCAPtimony's Asset table
+    belongs_to :asset
+    belongs_to_host
+  end
+
+ class Asset < ActiveRecord::Base
+    has_one :auditable_host
+    has_one :host, :through => :auditable_host
+  end
+
+  module ::Host
+    class Managed < ::Host::Base
+      has_one :auditable_host, :class_name => "::Scaptimony::AuditableHost", :foreign_key => :host_id
+    end
+  end
+end
