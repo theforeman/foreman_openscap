06ec0916df26942353d4cd459fbf9fa94d28e7ab
Šimon Lukašík <slukasik@redhat.com>
2014-12-12 14:06:38 +0100

    Fill in the dashboard with real data
    Relates to #11

--- app/services/scaptimony/policy_dashboard/data.rb
+++ app/services/scaptimony/policy_dashboard/data.rb
@@ -12,13 +12,18 @@ module Scaptimony::PolicyDashboard
     private
     attr_writer :report
 
+    def hosts
+      @hosts ||= Host.authorized(:view_hosts, Host)
+    end
+
     def fetch_data
       report.update(
-        {:compliant_hosts => 3,
-         :incompliant_hosts => 4,
-         :report_delayed => 5,
-         :report_missing => 6,
-         :unassigned_hosts => 12,
+        {:compliant_hosts => @policy.assets.hosts.comply_with(@policy).count,
+         :incompliant_hosts => @policy.assets.hosts.incomply_with(@policy).count,
+         :inconclusive_hosts => @policy.assets.hosts.inconclusive_with(@policy).count,
+         :report_missing => @policy.assets.hosts.policy_reports_missing(@policy).count,
+         :assigned_hosts => @policy.assets.hosts.count,
+         :unassigned_hosts => hosts.count - @policy.assets.hosts.count
         })
     end
   end
