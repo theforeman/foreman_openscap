82abcba26385f75c91e0311f6058526d276f7125
Shlomi Zadok <shlomi@ben-hanna.com>
2015-01-01 15:01:48 +0200

    validations on hostgroup

--- app/models/scaptimony/policy_hostgroup.rb
+++ app/models/scaptimony/policy_hostgroup.rb
@@ -3,5 +3,7 @@ module ::Scaptimony
     attr_accessible :hostgroup_id, :policy_id
     belongs_to :policy
     belongs_to :hostgroup
+
+    validates :hostgroup_id, :uniqueness => {:scope => :policy_id}
   end
 end
