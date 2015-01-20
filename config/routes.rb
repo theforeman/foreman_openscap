06136908d7741e01724e02e5ccab6372b8ed7b12
Šimon Lukašík <slukasik@redhat.com>
2015-01-20 14:59:42 +0100

    Change policy param to policy_id
    Required since OpenSCAP/SCAPtimony#16.

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
     scope "(:apiv)", :module => :v2, :defaults => {:apiv => 'v2'},
           :apiv => /v1|v2/, :constraints => ApiConstraints.new(:version => 2) do
       namespace :compliance do
-        post 'arf_reports/:cname/:policy/:date', \
+        post 'arf_reports/:cname/:policy_id/:date', \
               :constraints => { :cname => /[^\/]+/ }, :to => 'arf_reports#create'
       end
     end
