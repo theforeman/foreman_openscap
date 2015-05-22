b1bb2765429752cbae04579bb3a0247927a9a2d9
Marek Hulán <ares@igloonet.cz>
2015-05-22 08:57:45 +0200

    Merge pull request #104 from shlomizadok/fix_10549
    Fixes #10549 - show host name only if host exists
    (cherry picked from commit 599ab920f2f09433c922c63532cbb4464dc1e402)

--- app/helpers/concerns/foreman_openscap/hosts_helper_extensions.rb
+++ app/helpers/concerns/foreman_openscap/hosts_helper_extensions.rb
@@ -4,10 +4,15 @@ module ForemanOpenscap
 
     included do
       alias_method_chain :multiple_actions, :scap
+      alias_method_chain :name_column, :scap
     end
 
     def multiple_actions_with_scap
       multiple_actions_without_scap << [_('Assign Compliance Policy'), select_multiple_hosts_scaptimony_policies_path]
+    end
+
+    def name_column_with_scap(record)
+      record.nil? ? _('Host is deleted') : name_column_without_scap(record)
     end
   end
 end
