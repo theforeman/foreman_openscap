6a74fd39bc041be393ce3cf496727edc080012d9
Marek Hulan <mhulan@redhat.com>
2015-06-02 13:30:07 +0200

    Fixes asset factory to not create Host on definition load

--- test/factories/asset_factory.rb
+++ test/factories/asset_factory.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :asset, :class => Scaptimony::Asset do |f|
-    f.assetable_id FactoryGirl.create(:host).id
+    f.assetable_id { FactoryGirl.create(:host).id }
     f.assetable_type 'Host::Base'
   end
 end
