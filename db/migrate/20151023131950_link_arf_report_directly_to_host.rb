a31ebb71bb4a9e3dee61350b9c47c98e7aa08034
Ondrej Prazak <oprazak@redhat.com>
2015-12-07 16:41:33 +0100

    Fixes #12734 - Migration performance improvement

--- db/migrate/20151023131950_link_arf_report_directly_to_host.rb
+++ db/migrate/20151023131950_link_arf_report_directly_to_host.rb
@@ -1,17 +1,21 @@
 class LinkArfReportDirectlyToHost < ActiveRecord::Migration
   def up
-    ForemanOpenscap::ArfReport.all.each do |report|
-      asset = ForemanOpenscap::Asset.where(:id => report.host_id).first
-      report.host_id = asset.host.id
-      report.save!
+    ForemanOpenscap::ArfReport.find_in_batches do |batch|
+      batch.each do |report|
+        asset = ForemanOpenscap::Asset.find(:id => report.host_id)
+        report.host_id = asset.host.id
+        report.save!
+      end
     end
   end
 
   def down
-    ForemanOpenscap::ArfReport.all.each do |report|
-      asset = ForemanOpenscap::Asset.where(:assetable_id => report.host_id, :assetable_type => 'Host::Base').first
-      report.host_id = asset.id
-      report.save!
+    ForemanOpenscap::ArfReport.find_in_batches do |batch|
+      batch.all.each do |report|
+        asset = ForemanOpenscap::Asset.find(:assetable_id => report.host_id, :assetable_type => 'Host::Base')
+        report.host_id = asset.id
+        report.save!
+      end
     end
   end
 end
