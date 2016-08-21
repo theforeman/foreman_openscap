42b0a14a502bbc78f0ef8dc42a14e5aa90aa7851
Ranjan Kumar <ranjankumar188@gmail.com>
2016-08-21 13:31:59 +0530

    Fixes #16210 - Handling hostgroup deletion.

--- app/models/concerns/foreman_openscap/hostgroup_extensions.rb
+++ app/models/concerns/foreman_openscap/hostgroup_extensions.rb
@@ -3,7 +3,7 @@ module ForemanOpenscap
     extend ActiveSupport::Concern
 
     included do
-      has_one :asset, :as => :assetable, :class_name => "::ForemanOpenscap::Asset"
+      has_one :asset, :as => :assetable, :class_name => "::ForemanOpenscap::Asset", dependent: :destroy
       has_many :asset_policies, :through => :asset, :class_name => "::ForemanOpenscap::AssetPolicy"
       has_many :policies, :through => :asset_policies, :class_name => "::ForemanOpenscap::Policy"
     end
--- test/unit/policy_test.rb
+++ test/unit/policy_test.rb
@@ -17,4 +17,16 @@ class PolicyTest < ActiveSupport::TestCase
     assert_equal 2, policy.hostgroups.count
     assert policy.hostgroups.include?(hg2)
   end
+
+  test "should remove associated hostgroup" do
+    ForemanOpenscap::Policy.any_instance.stubs(:find_scap_puppetclass).returns(FactoryGirl.create(:puppetclass, :name => 'foreman_scap_client'))
+    ForemanOpenscap::Policy.any_instance.stubs(:populate_overrides)
+    hg1 = FactoryGirl.create(:hostgroup)
+    asset = FactoryGirl.create(:asset, :assetable_id => hg1.id, :assetable_type => 'Hostgroup')
+    policy = FactoryGirl.create(:policy, :assets => [asset])
+    policy.hostgroup_ids = [hg1].map(&:id)
+    policy.save!
+    hg1.destroy
+    assert_equal 0, policy.hostgroups.count
+  end
 end
