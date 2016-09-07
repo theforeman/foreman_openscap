8197c2ef37768148313374d48b71333e818392e5
Shlomi Zadok <shlomi@ben-hanna.com>
2016-09-07 12:38:53 +0300

    Fixes #16192 - Remove license from files

--- app/controllers/policy_dashboard_controller.rb
+++ app/controllers/policy_dashboard_controller.rb
@@ -1,13 +1,3 @@
-#
-# Copyright (c) 2014 Red Hat Inc.
-#
-# This software is licensed to you under the GNU General Public License,
-# version 3 (GPLv3). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
-# along with this software; if not, see http://www.gnu.org/licenses/gpl.txt
-#
-
 class PolicyDashboardController < ApplicationController
   before_filter :prefetch_data, :only => :index
 
--- lib/foreman_openscap/helper.rb
+++ lib/foreman_openscap/helper.rb
@@ -1,13 +1,3 @@
-#
-# Copyright (c) 2014 Red Hat Inc.
-#
-# This software is licensed to you under the GNU General Public License,
-# version 3 (GPLv3). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
-# along with this software; if not, see http://www.gnu.org/licenses/gpl.txt
-#
-
 module ForemanOpenscap::Helper
   def self.get_asset(cname, policy_id)
     asset = find_host_by_name_or_uuid(cname).get_asset
