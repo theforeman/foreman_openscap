1e596e42de8e656c8a9108a6ffe1096a322789c2
Ondrej Prazak <oprazak@redhat.com>
2017-01-17 17:20:42 +0100

    Fixes #17240 - Add spool failure warning

---
+++ test/functional/openscap_proxies_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_plugin_helper'
+
+class OpenscapProxiesControllerTest < ActionController::TestCase
+  include ActionView::Helpers::DateHelper
+
+  test "should render spool error" do
+    spool_error = { "timestamp" => 1_487_144_633.951_368, "level" => "ERROR", "message"=> "Failed to parse Arf Report in test" }
+    OpenscapProxiesController.any_instance.stubs(:find_spool_error).returns(spool_error)
+    proxy = FactoryGirl.create(:openscap_proxy)
+    get :openscap_spool, { :id => proxy.id }, set_session_user
+    assert_template :partial => 'smart_proxies/_openscap_spool'
+    assert @response.body.match(time_ago_in_words(Time.at(spool_error["timestamp"])))
+  end
+end
