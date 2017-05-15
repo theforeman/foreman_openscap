25853b7798ae8278f346a7e16be3346ec4c818c5
Ondrej Prazak <oprazak@redhat.com>
2017-05-15 15:07:04 +0200

    Fixes #19552 - Fix search for hosts without arf report

--- app/services/foreman_openscap/policy_dashboard/data.rb
+++ app/services/foreman_openscap/policy_dashboard/data.rb
@@ -17,13 +17,14 @@ module ForemanOpenscap::PolicyDashboard
     end
 
     def fetch_data
+      assigned_count = Host::Managed.assigned_to_policy(@policy).count
       report.update(
         {:compliant_hosts => Host::Managed.comply_with(@policy).count,
          :incompliant_hosts => Host::Managed.incomply_with(@policy).count,
          :inconclusive_hosts => Host::Managed.inconclusive_with(@policy).count,
          :report_missing => Host::Managed.policy_reports_missing(@policy).count,
-         :assigned_hosts => @policy.assets.hosts.count,
-         :unassigned_hosts => hosts.count - @policy.assets.hosts.count}
+         :assigned_hosts => assigned_count,
+         :unassigned_hosts => hosts.count - assigned_count}
       )
     end
   end
--- test/factories/asset_factory.rb
+++ test/factories/asset_factory.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
     f.assetable_id { FactoryGirl.create(:host).id }
     f.assetable_type 'Host::Base'
   end
+
+  factory :asset_policy, :class => ForemanOpenscap::AssetPolicy do |f|
+    f.asset_id nil
+    f.policy_id nil
+  end
 end
