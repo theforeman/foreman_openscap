8347e04c5d794d7236c1e91b968cb3a01d67a3fc
Ondrej Prazak <oprazak@redhat.com>
2017-05-16 11:46:29 +0200

    Fixes #19562 - Fix failing tests after minitest update

--- test/factories/compliance_host_factory.rb
+++ test/factories/compliance_host_factory.rb
@@ -6,16 +6,14 @@ FactoryGirl.define do
   factory :openscap_proxy, :class => SmartProxy do
     sequence(:name) { |n| "proxy#{n}" }
     sequence(:url) { |n| "https://somewhere#{n}.net:8443" }
-    features [FactoryGirl.create(:openscap_feature)]
+    features { |sp| [sp.association(:openscap_feature)] }
   end
 
   factory :compliance_host, :class => Host::Managed do
     sequence(:name) { |n| "host#{n}" }
     sequence(:hostname) { |n| "hostname#{n}" }
     root_pass 'xybxa6JUkz63w'
-    openscap_proxy do
-      FactoryGirl.create(:openscap_proxy)
-    end
+    openscap_proxy { SmartProxy.unscoped.with_features('Openscap').first || FactoryGirl.create(:openscap_proxy) }
     policies []
   end
 end
