330f4e1c4f7caab6b6d81c87bd3d4c6a7559e5ee
Marek Hulan <mhulan@redhat.com>
2017-08-21 16:32:33 +0200

    Fixes #16490 - extend hostgroup and host API

--- app/overrides/hosts/form/select_openscap_proxy.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "hosts/_form",
-                     :name => "openscap_proxy",
-                     :insert_bottom => "#primary",
-                     :partial => "compliance_hosts/openscap_proxy")
