2fda7db006d10aaa846ba280bdb10ac1eea53cec
Ondrej Prazak <oprazak@redhat.com>
2017-08-21 12:57:57 +0200

    Fixes #20666 - Check for profile and content mismatch in policy

--- test/factories/policy_factory.rb
+++ test/factories/policy_factory.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     sequence(:name) { |n| "policy#{n}" }
     period 'weekly'
     weekday 'monday'
-    scap_content
-    scap_content_profile
+    scap_content { FactoryGirl.create(:scap_content) }
+    scap_content_profile { FactoryGirl.create(:scap_content_profile, :scap_content => scap_content) }
     tailoring_file nil
     tailoring_file_profile nil
     day_of_month nil
