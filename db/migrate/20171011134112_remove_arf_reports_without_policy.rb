0f857e0c1bd25d834bf1e5e60979287256f886dc
Ondrej Prazak <oprazak@redhat.com>
2017-10-05 14:22:40 +0200

    Fixes #21209 - Delete reports when deleting a policy

---
+++ db/migrate/20171011134112_remove_arf_reports_without_policy.rb
@@ -0,0 +1,8 @@
+class RemoveArfReportsWithoutPolicy < ActiveRecord::Migration
+  def up
+    ids_to_keep = ForemanOpenscap::ArfReport.unscoped.all.joins(:policy_arf_report).pluck(:id)
+    ForemanOpenscap::ArfReport.unscoped.where.not(:id => ids_to_keep).find_in_batches do |batch|
+      batch.map(&:destroy!)
+    end
+  end
+end
