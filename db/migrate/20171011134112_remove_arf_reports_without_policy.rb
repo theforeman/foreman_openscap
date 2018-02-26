009817638e99ec94508a4b96c6c441799290cb82
Ondrej Prazak <oprazak@redhat.com>
2018-02-26 10:45:31 +0100

    Fixes #22668 - Always delete report records
    (cherry picked from commit 7c4260dd3edc999f663964ea4724e79ff6613b3b)

--- db/migrate/20171011134112_remove_arf_reports_without_policy.rb
+++ db/migrate/20171011134112_remove_arf_reports_without_policy.rb
@@ -1,8 +1,10 @@
 class RemoveArfReportsWithoutPolicy < ActiveRecord::Migration
   def up
-    ids_to_keep = ForemanOpenscap::ArfReport.unscoped.all.joins(:policy_arf_report).pluck(:id)
-    ForemanOpenscap::ArfReport.unscoped.where.not(:id => ids_to_keep).find_in_batches do |batch|
-      batch.map(&:destroy!)
+    User.as_anonymous_admin do
+      ids_to_keep = ForemanOpenscap::ArfReport.unscoped.all.joins(:policy_arf_report).pluck(:id)
+      ForemanOpenscap::ArfReport.unscoped.where.not(:id => ids_to_keep).find_in_batches do |batch|
+        batch.map(&:destroy!)
+      end
     end
   end
 end
