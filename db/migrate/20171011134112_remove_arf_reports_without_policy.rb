9b22af60e1002793d7919604133a5f75b71d032c
Marek Hulan <mhulan@redhat.com>
2018-03-12 15:47:39 +0100

    Refs #22668 - use admin only on seeded database

--- db/migrate/20171011134112_remove_arf_reports_without_policy.rb
+++ db/migrate/20171011134112_remove_arf_reports_without_policy.rb
@@ -1,10 +1,18 @@
 class RemoveArfReportsWithoutPolicy < ActiveRecord::Migration[4.2]
   def up
-    User.as_anonymous_admin do
-      ids_to_keep = ForemanOpenscap::ArfReport.unscoped.all.joins(:policy_arf_report).pluck(:id)
-      ForemanOpenscap::ArfReport.unscoped.where.not(:id => ids_to_keep).find_in_batches do |batch|
-        batch.map(&:destroy!)
+    if User.unscoped.find_by(:login => User::ANONYMOUS_ADMIN)
+      User.as_anonymous_admin do
+        delete_reports
       end
+    else
+      delete_reports
+    end
+  end
+
+  def delete_reports
+    ids_to_keep = ForemanOpenscap::ArfReport.unscoped.all.joins(:policy_arf_report).pluck(:id)
+    ForemanOpenscap::ArfReport.unscoped.where.not(:id => ids_to_keep).find_in_batches do |batch|
+      batch.map(&:destroy!)
     end
   end
 end
