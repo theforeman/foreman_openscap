6deb9cb581f1befa9f60ad86bda373b117c96a6f
Trey Dockendorf <tdockendorf@osc.edu>
2018-05-08 13:09:43 -0400

    Fixes #23544 - Edit multiple host's OpenSCAP Proxy

--- app/helpers/concerns/foreman_openscap/hosts_helper_extensions.rb
+++ app/helpers/concerns/foreman_openscap/hosts_helper_extensions.rb
@@ -2,7 +2,8 @@ module ForemanOpenscap
   module HostsHelperExtensions
     def multiple_actions
       super + [[_('Assign Compliance Policy'), select_multiple_hosts_policies_path],
-                                       [_('Unassign Compliance Policy'), disassociate_multiple_hosts_policies_path]]
+                                       [_('Unassign Compliance Policy'), disassociate_multiple_hosts_policies_path],
+                                       [_('Change OpenSCAP Proxy'), select_multiple_openscap_proxy_hosts_path]]
     end
 
     def name_column(record)
