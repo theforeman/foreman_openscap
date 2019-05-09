561e9385224a8b4e1fe0cd9aa3998c5cc4e8a85e
Ondřej Pražák <xprazak2@users.noreply.github.com>
2019-05-09 13:26:54 +0200

    Fixes #26667 - Add pagination to policy dashboard (#380)

--- app/controllers/policy_dashboard_controller.rb
+++ app/controllers/policy_dashboard_controller.rb
@@ -8,5 +8,10 @@ class PolicyDashboardController < ApplicationController
     @policy = ::ForemanOpenscap::Policy.find(params[:id])
     dashboard = ForemanOpenscap::PolicyDashboard::Data.new(@policy, params[:search])
     @report = dashboard.report
+    @latest_reports = ForemanOpenscap::ArfReport
+                      .includes(:host)
+                      .of_policy(@policy.id)
+                      .latest
+                      .paginate(:page => params[:page], :per_page => params[:per_page])
   end
 end
