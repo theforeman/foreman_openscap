9ca5db8c4177f81fde55466fcee5e5f52657df56
Ondrej Prazak <oprazak@redhat.com>
2019-06-04 10:58:26 +0200

    Fixes #26943 - Fix arf report bulk delete

--- app/assets/javascripts/foreman_openscap/arf_reports.js
+++ app/assets/javascripts/foreman_openscap/arf_reports.js
@@ -1,5 +1,27 @@
+function readFromCookie() {
+  try {
+    var r = $.cookie(cookieName);
+    if (r) {
+      return $.parseJSON(r);
+    }
+    return [];
+  } catch (err) {
+    removeCookie();
+    return [];
+  }
+}
+
+function removeCookie() {
+  $.removeCookie(cookieName);
+}
+
+var cookieName = `_ForemanSelected${window.location.pathname.replace(
+  /\//,
+  ''
+)}`;
+
 function buildArfModal(element, url) {
-  var url = url + "?" + $.param({arf_report_ids: $.foremanSelectedHosts});
+  var url = url + "?" + $.param({arf_report_ids: readFromCookie()});
   var title = $(element).attr('data-dialog-title');
   $('#confirmation-modal .modal-header h4').text(title);
   $("#confirmation-modal .modal-body").load(url + " #content",
