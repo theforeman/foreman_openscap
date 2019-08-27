a72fd61e804d819f56ea31de0405ac76d94bc0dc
Ondrej Prazak <oprazak@redhat.com>
2019-08-27 14:28:37 +0200

    Fixes #27721 - Use DSL to register compliance link

--- app/overrides/hosts/overview/host_compliance_status.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "hosts/show",
-                     :name => "add_compliance_link_to_host",
-                     :insert_bottom => 'td:last',
-                     :partial => 'compliance_hosts/compliance_status')
