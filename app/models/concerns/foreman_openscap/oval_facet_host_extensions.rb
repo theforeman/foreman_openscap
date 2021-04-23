bd21ce937a815e423537383959e8407614ba6e7c
Ondrej Prazak <oprazak@redhat.com>
2021-04-23 13:32:10 +0200

    Refs #31380 - Improve OVAL status text

--- app/models/concerns/foreman_openscap/oval_facet_host_extensions.rb
+++ app/models/concerns/foreman_openscap/oval_facet_host_extensions.rb
@@ -2,13 +2,21 @@ module ForemanOpenscap
   module OvalFacetHostExtensions
     extend ActiveSupport::Concern
 
-    ::Host::Managed::Jail.allow :oval_policies_enc, :oval_policies_enc_raw, :cves
+    ::Host::Managed::Jail.allow :oval_policies_enc, :oval_policies_enc_raw, :cves, :cves_without_errata
 
     included do
       has_many :oval_policies, :through => :oval_facet, :class_name => 'ForemanOpenscap::OvalPolicy'
 
       has_many :host_cves, :class_name => 'ForemanOpenscap::HostCve', :foreign_key => :host_id
       has_many :cves, :through => :host_cves, :class_name => 'ForemanOpenscap::Cve', :source => :cve
+    end
+
+    def cves_without_errata
+      cves.where(:has_errata => false)
+    end
+
+    def cves_with_errata
+      cves.where(:has_errata => true)
     end
 
     def combined_oval_policies
