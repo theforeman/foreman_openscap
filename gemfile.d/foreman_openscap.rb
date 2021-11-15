e5c64038856653d30af0f2f22fa884a1c0e99a2c
Ondrej Prazak <oprazak@redhat.com>
2021-11-15 11:29:47 +0100

    wip

--- gemfile.d/foreman_openscap.rb
+++ gemfile.d/foreman_openscap.rb
@@ -1,3 +1,3 @@
 # Add anible for tests on Jenkins
-gem 'foreman_ansible'
-gem 'foreman_ansible_core'
+gem 'foreman_ansible', git: 'https://github.com/theforeman/foreman_ansible'
+gem 'foreman_puppet', git: 'https://github.com/theforeman/foreman_puppet'
