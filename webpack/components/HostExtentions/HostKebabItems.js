c7216fbddf5c886b859982c6f99cc280e93679ff
Ron Lavi <1ronlavi@gmail.com>
2023-03-08 14:58:02 +0200

    Refs #36133 - disable when there's no report

--- webpack/components/HostExtentions/HostKebabItems.js
+++ webpack/components/HostExtentions/HostKebabItems.js
@@ -3,24 +3,38 @@ import { useSelector } from 'react-redux';
 import { DropdownItem } from '@patternfly/react-core';
 import { SecurityIcon } from '@patternfly/react-icons';
 import { selectAPIResponse } from 'foremanReact/redux/API/APISelectors';
+import { useAPI } from 'foremanReact/common/hooks/API/APIHooks';
 import { translate as __ } from 'foremanReact/common/I18n';
 import { HOST_DETAILS_KEY } from 'foremanReact/components/HostDetails/consts';
 import { foremanUrl } from 'foremanReact/common/helpers';
 
 const HostKebabItems = () => {
-  const { id } = useSelector(state =>
+  const { name, id } = useSelector(state =>
     selectAPIResponse(state, HOST_DETAILS_KEY)
   );
 
+  const ARFReportsAPIPath = name
+    ? `/api/v2/compliance/arf_reports?search=host+%3D+${name}`
+    : null;
+
+  const {
+    response: { results = [] },
+  } = useAPI('get', ARFReportsAPIPath);
+
   const compliancePath = foremanUrl(`/compliance/hosts/${id}`);
+
+  const isDisabled = results.length === 0;
 
   return (
     <DropdownItem
       ouiaId="compliance-dropdown-item"
+      key="compliance-report"
       icon={<SecurityIcon />}
       href={compliancePath}
       target="_blank"
       rel="noreferrer"
+      isAriaDisabled={isDisabled}
+      tooltip={isDisabled && __("There's no available report for this host")}
     >
       {__('Compliance')}
     </DropdownItem>
