3bfce3d512670b42bc362ad070095a1fd919c163
Oleh Fedorenko <ofedoren@redhat.com>
2024-05-15 13:53:54 +0000

    Fixes #37464 - Support Zeitwerk loader

--- db/migrate/20201217161511_add_url_to_oval_content.rb
+++ db/migrate/20201217161511_add_url_to_oval_content.rb
@@ -1,4 +1,4 @@
-class AddUrlToOvalContent < ActiveRecord::Migration[6.0]
+class AddURLToOvalContent < ActiveRecord::Migration[6.0]
   def change
     add_column :foreman_openscap_oval_contents, :url, :string
   end
