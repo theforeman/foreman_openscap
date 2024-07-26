830ebef522ee082cc6dfa6e109dc6951d477c478
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-07-26 16:02:36 +0200

    Use documentation URL helper from core
    Foreman 3.9.1+ has built in support to link to docs. This simplifies the
    code in this module.

--- app/helpers/foreman_openscap_helper.rb
+++ app/helpers/foreman_openscap_helper.rb
@@ -2,26 +2,10 @@ require 'foreman_openscap/version'
 
 module ForemanOpenscapHelper
   def scap_doc_button(section)
-    documentation_button(section, root_url: scap_doc_url)
+    documentation_button('Managing_Security_Compliance', type: 'docs', chapter: section)
   end
 
   def scap_doc_url(section = '')
-    return scap_root_url if section.empty?
-
-    documentation_url(section, root_url: scap_root_url)
-  end
-
-  private
-
-  def doc_flavor
-    ForemanOpenscap.with_katello? ? 'katello' : 'foreman-el'
-  end
-
-  def scap_root_url
-    @scap_root_url ||= begin
-      version = SETTINGS[:version]
-      version = version.tag == 'develop' ? 'nightly' : version.short
-      "https://docs.theforeman.org/#{version}/Managing_Security_Compliance/index-#{doc_flavor}.html#"
-    end
+    documentation_url('Managing_Security_Compliance', type: 'docs', chapter: section)
   end
 end
