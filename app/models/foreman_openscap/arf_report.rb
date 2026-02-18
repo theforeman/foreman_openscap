ff44e9d75262722bee76c90463e08851191f85d6
Adam Ruzicka <aruzicka@redhat.com>
2026-02-18 14:17:19 +0100

    Fix json warnings in tests
    JSON.fast_generate is deprecated and will be removed in json  3.0.0,
    just use JSON.generate

--- app/models/foreman_openscap/arf_report.rb
+++ app/models/foreman_openscap/arf_report.rb
@@ -230,7 +230,7 @@ module ForemanOpenscap
     def self.fixes(raw_fixes)
       return if raw_fixes.empty?
 
-      JSON.fast_generate(raw_fixes)
+      JSON.generate(raw_fixes)
     end
 
     def self.update_msg_with_changes(msg, incoming_data)
